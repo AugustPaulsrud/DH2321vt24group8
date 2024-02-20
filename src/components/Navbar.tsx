184bb1050c58bb4db5c922c180d9908b2b2b84ba
Marcus <marcusko.work@gmail.com>
2024-02-20 22:41:43 +0100

    Added Dropdown for selecting studies

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
     let [open, setOpen] = useState(false);
 
     return (
-        <div className='shadow-md w-full top-0 left-0 p-2'>
+        <div className='shadow-md w-full fixed top-0 left-0'>
             <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
             <div className='font-bold text-2xl cursor-pointer flex items-center gap-1'>
                 <span>DH2321 - Group 8</span>
@@ -23,13 +23,13 @@ const Navbar = () => {
                     open ? <XMarkIcon/> : <Bars3BottomRightIcon />
                 }
             </div>
-            <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in 
-                ${ open ? 'md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white z-10 left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in' : 'top-[-490px]'}`}>
+            <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-12' : 'top-[-490px]'}`}>
                 {
                     Links.map((link, index) => (
                         <CustomLink key={index} to={link.link + link.name} className='text-gray-800 hover:text-blue-400 duration-500'>{link.name}</CustomLink>
                     ))
                 }
+                
             </ul>
            </div>
         </div>
--- src/pages/Visuals.tsx
+++ src/pages/Visuals.tsx
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { useState , useEffect} from "react";
 import { ScatterplotSimple } from "../components/ScatterplotSimple";
 import Plot3D from "../components/Plot3D";
 
 const Visuals = () => {
-    const [is3D, setIs3D] = React.useState(false); // State to track whether to render 2D or 3D graph
+    const [is3D, setIs3D] = useState(false); // State to track whether to render 2D or 3D graph
+    const [selectedCsvFile1, setSelectedCsvFile1] = useState("");
+    const [selectedCsvFile2, setSelectedCsvFile2] = useState("");
+
+    // const [csvFiles, setCsvFiles] = useState<string[]>([]);
+
+    // Manually provided list of CSV file names for demonstration
+    const csvFiles = ["EVDa_SimCaTip_Ale0003", 
+                    "EVDb_SimCaPlus_Ale0004", 
+                    "EVDb_SimCaPlus_Ale0005", 
+                    "EVDb_SimCaPlus_Mario0006", 
+                    "EVDb_SimCaPlus_Mario0007"
+                ]; 
+    
+    // Placeholder Code for dynamically retrieving CSV files from server
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         try {
+    //             const response = await fetch(`/data/csv/`);
+    //             const fileNames = await response.text(); // Ensure fileNames is of type string
+    //             const filesArray = fileNames.split('\n').filter(name => name.trim() !== ''); // Split by newline and filter out empty strings
+    //             setCsvFiles(filesArray);
+    //         } catch (error) {
+    //             console.error('Error fetching CSV files:', error);
+    //         }
+    //     };
+
+    //     fetchData();
+    // }, []);
 
     const toggleGraph = () => {
         setIs3D(!is3D); // Toggle between 2D and 3D graphs
     };
 
     return (
-        <div className="flex flex-col p-4">
-            <div className="flex space-x-4">
+        <div className="flex flex-col p-4 w-full">
+            <div className="flex space-x-4 w-full mb-4">
                 <button
                     className={`px-4 py-2 rounded-md ${is3D ? 'bg-gray-300' : 'bg-blue-500 text-white'}`}
                     onClick={() => setIs3D(false)}
@@ -25,9 +53,45 @@ const Visuals = () => {
                     Show 3D Graph
                 </button>
             </div>
-            { is3D ? <Plot3D width={800} height={600} csv_file={"EVDa_SimCaTip_Ale0003"} /> : 
-            <ScatterplotSimple width={600} height={600} csv_file={"EVDa_SimCaTip_Ale0003"} /> }
-            
+            { is3D ? 
+            ( 
+                <div>
+                    <Plot3D width={800} height={600} csv_file={"EVDa_SimCaTip_Ale0003"} />  
+                </div>
+            ) : (
+                <div className="flex flex-col md:flex-row w-full justify-center items-center md:divide-x-2 max-md:divide-y-2">
+                    <div className="[&>*]:mx-3">
+                        <div className="pt-20 relative inline-flex top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                            <select
+                                className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                                onChange={(e) => setSelectedCsvFile1(e.target.value)}
+                                value={selectedCsvFile1}
+                            >
+                                <option value="">Select CSV file</option>
+                                {csvFiles.map((file, index) => (
+                                    <option key={index} value={file}>{file}</option>
+                                ))}
+                            </select>
+                        </div>
+                        <ScatterplotSimple width={600} height={600} csv_file={selectedCsvFile1} /> 
+                    </div>
+                    <div className="[&>*]:mx-3">
+                        <div className="pt-20 relative inline-flex top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                            <select
+                                className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+                                onChange={(e) => setSelectedCsvFile2(e.target.value)}
+                                value={selectedCsvFile2}
+                            >
+                                <option value="">Select CSV file</option>
+                                {csvFiles.map((file, index) => (
+                                    <option key={index} value={file}>{file}</option>
+                                ))}
+                            </select>
+                        </div>
+                        <ScatterplotSimple width={600} height={600} csv_file={selectedCsvFile2} /> 
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
