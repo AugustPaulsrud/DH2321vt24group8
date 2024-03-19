9b13891ce6c7fadb7a79f011ee4313bc7173508c
Marcus <marcusko.work@gmail.com>
2024-03-19 17:45:09 +0100

    Updated Info Page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -13,6 +13,11 @@ const About = () => {
                     EVD is a risky procedure as improper placement of the catheter could cause intracranial haemorrhaging or permanent brain injuries to the patient. 
                     With this in mind, 4DMotion was created as a tool to allow researchers and future surgeons in training to visualize the procedures of EVD surgery by referencing data of previous surgical trials captured using motion capture.
                 </p>
+                <div className="flex justify-center items-center my-16 relative animate-fade-up">
+                    <div className="h-2 w-2 bg-blue-900 rounded-full mx-2"></div>
+                    <div className="h-2 w-2 bg-blue-900 rounded-full mx-2"></div>
+                    <div className="h-2 w-2 bg-blue-900 rounded-full mx-2"></div>
+                </div>
                 <h1 className="text-2xl md:text-3xl font-bold text-center text-gray-800 mb-4 animate-fade-up">Data</h1>
                 <p className="text-gray-600 text-center mb-8 my-4 animate-fade-up">
                     The data for multiple procedures/trials from different settings (i.e. location and system to capture the data, amount of markers, etc.) was given to us from <b>Alessandro Iop</b>. We selected <b>5</b> different trials from <b>one</b> setting (the setting which had the least number of markers, for simplicity). The selected raw data can be found 
@@ -20,12 +25,22 @@ const About = () => {
                     The subsampled data can be found <a href="https://github.com/AugustPaulsrud/DH2321vt24group8/tree/main/public/data/csv_downsampled" className="text-blue-500"> here</a> and the metadata <a href="https://github.com/AugustPaulsrud/DH2321vt24group8/blob/main/public/data/metadata.csv" className="text-blue-500"> here</a>. <br/><br/>
                     <b>Note:</b> The parsed data includes angle but this was not used by the visual mappings due to time constraints.
                 </p>
+                <div className="flex justify-center items-center my-16 relative animate-fade-up">
+                    <div className="h-2 w-2 bg-blue-900 rounded-full mx-2"></div>
+                    <div className="h-2 w-2 bg-blue-900 rounded-full mx-2"></div>
+                    <div className="h-2 w-2 bg-blue-900 rounded-full mx-2"></div>
+                </div>
                 <h1 className="text-2xl md:text-3xl font-bold text-center text-gray-800 mb-4 animate-fade-up">Credits/Acknowledgements</h1>
                 <p className="text-gray-600 text-center mb-8 my-4 animate-fade-up">
                     We would like to acknowledge <b>Alessandro Iop</b> for proposing the idea, sharing the data, giving us feedback and supporting us throughout the project. 
                     We also want to thank <b>Mario Romero Vega</b> for taking time to review our visualization tool and giving us valuable feedback as well as inspiring us to learn more about information visualization. 
                     Lastly we want to thank <b>Henrik Garde</b> for letting us do a semi-structured interview and giving us a chance to test how well the visualization worked for generating insights as well as giving us important feedback. 
                 </p>
+                <div className="flex justify-center items-center my-16 relative animate-fade-up">
+                    <div className="h-2 w-2 bg-blue-900 rounded-full mx-2"></div>
+                    <div className="h-2 w-2 bg-blue-900 rounded-full mx-2"></div>
+                    <div className="h-2 w-2 bg-blue-900 rounded-full mx-2"></div>
+                </div>
                 <h1 className="text-2xl md:text-3xl font-bold text-center text-gray-800 mb-4 animate-fade-up">Citations/References/Source</h1>
                 <p className="text-gray-600 text-left mb-8 my-4 animate-fade-up">
                     <ul className="list-disc ml-6">
@@ -34,6 +49,9 @@ const About = () => {
                         <li className="p-2">[3] Y. Tashiro and T. Saitoh, "A Study on Motion Visualization System Using Motion Capture Data," 17th International Conference on Artificial Reality and Telexistence (ICAT 2007), Esbjerg, Denmark, 2007, pp. 314-315, doi: 10.1109/ICAT.2007.53.</li>
                         <li className="p-2">[4] Wippich, M. (2023). Effects of Virtual Reality and Trajectory Visualization on Neurosurgical Training (Dissertation). Retrieved from https://urn.kb.se/resolve?urn=urn:nbn:se:kth:diva-343144</li>
                         <li className="p-2">[5] Steinarsson, S. (2013). Downsampling time series for visual representation (Doctoral dissertation).</li>
+                        <li className="p-2">[6] D3.js. (2023). Data-Driven Documents (Version 7.0.0) [Software]. GitHub. https://github.com/d3/d3</li>
+                        <li className="p-2">[7] Plotly Technologies Inc. (2023). Plotly.js (Version 2.0.0) [Software]. Plotly. https://plotly.com/javascript/ </li>
+                        <li className="p-2">[8] Sanderson, G. (2018, October 26). Quaternions and 3D rotation, explained interactively. YouTube. https://www.youtube.com/watch?v=zjMuIxRvygQ&ab_channel=3Blue1Brown</li>
                         <li className="p-2">The source code of the project can be found <a href="https://github.com/AugustPaulsrud/DH2321vt24group8" className="text-blue-500">here</a>.</li>
                     </ul>
                 </p>
